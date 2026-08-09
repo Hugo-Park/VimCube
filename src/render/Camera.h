4f0d04a15bce7ce0a01d6f3b7e335dd7dc658e2a
Hugo-Park <244296983+Hugo-Park@users.noreply.github.com>
2026-08-09 22:34:13 +0900

    Add Camera header

---
+++ src/render/Camera.h
@@ -0,0 +1,63 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+#include "../geometry/GeoTypes.h"
+
+namespace vimcube::camera {
+
+    enum class ProjectionMode { ISOMETRIC, PERSPECTIVE };
+
+    class Camera {
+    private:
+
+        // Projection Mode
+        vimcube::camera::ProjectionMode projectionMode_ = vimcube::camera::ProjectionMode::ISOMETRIC;
+
+        // Basic Info
+        vimcube::geometry::Vector3d camPosition_;
+        vimcube::geometry::Vector3d target_;
+        vimcube::geometry::Vector3d up_;
+
+        // For Orbit Rotating
+        float azimuth_ = 0.0f;
+        float elevation_ = 0.0f;
+        float radius_ = 20.0f;
+
+        // For Isometric Mode
+        float zoom_ = 10.0f;
+
+        // For Perspective Mode
+        float fieldOfView_ = 90.0f;
+        float nearClip_ = 0.1f;
+        float farClip_ = 100.0f;
+    
+    public:
+        // Constructor
+        Camera(vimcube::camera::ProjectionMode projectionMode, vimcube::geometry::Vector3d camPosition, vimcube::geometry::Vector3d target, vimcube::geometry::Vector3d up) : projectionMode_(projectionMode), camPosition_(camPosition), target_(target), up_(up) {};
+
+        // Setter
+        void setProjectionMode(vimcube::camera::ProjectionMode projectionMode);
+        void setTarget(vimcube::geometry::Vector3d target);
+        
+        // Getter
+        vimcube::camera::ProjectionMode getProjectionMode() const;
+        vimcube::geometry::Vector3d getCamPosition() const;
+        vimcube::geometry::Vector3d getTarget() const;
+        float getZoom() const;  // Only for Isometric mode
+
+        // Methods
+        void updateCameraPosition();
+        void orbitUp(float angle);
+        void orbitDown(float angle);
+        void orbitLeft(float angle);
+        void orbitRight(float angle);
+
+        // Only for isometric mode
+        void zoomIn(float amount);
+        void zoomOut(float amount);
+
+        // Reset
+        void resetCamera();
+    };
+
+}
+#endif
